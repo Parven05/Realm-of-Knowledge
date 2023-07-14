e0385d8c2c4b6d2d813fce644dd28df2b5489b28
Parven <parvendranofficial05@gmail.com>
2023-07-14 14:46:20 +0800

    Finish Level 3

---
+++ Assets/Scripts/Others/LiftControllerUp.cs
@@ -0,0 +1,107 @@
+using UnityEngine;
+
+public class LiftControllerUp : MonoBehaviour
+{
+    [SerializeField] private float liftSpeed = 1f;
+    [SerializeField] private float delayTime = 5f;
+    [SerializeField] private Transform liftTop;
+
+    [SerializeField] private AudioSource liftAudioSource;
+
+    private Vector3 initialPosition;
+    private bool isPlayerInside;
+    private bool isLiftingUp;
+    private float delayTimer;
+
+    private void Start()
+    {
+        initialPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        if (isPlayerInside)
+        {
+            if (isLiftingUp)
+            {
+                if (transform.position.y < liftTop.position.y)
+                {
+                    float newY = Mathf.MoveTowards(transform.position.y, liftTop.position.y, liftSpeed * Time.deltaTime);
+                    transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+                }
+                else
+                {
+                    if (delayTimer <= 0f)
+                    {
+                        isLiftingUp = false;
+                        delayTimer = delayTime;
+                        PlayLiftSound();
+                    }
+                    else
+                    {
+                        delayTimer -= Time.deltaTime;
+                    }
+                }
+            }
+            else
+            {
+                if (transform.position.y > initialPosition.y)
+                {
+                    float newY = Mathf.MoveTowards(transform.position.y, initialPosition.y, liftSpeed * Time.deltaTime);
+                    transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+                }
+                else
+                {
+                    StopLiftSound();
+                }
+            }
+        }
+        else
+        {
+            if (transform.position.y > initialPosition.y)
+            {
+                float newY = Mathf.MoveTowards(transform.position.y, initialPosition.y, liftSpeed * Time.deltaTime);
+                transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+            }
+            else
+            {
+                StopLiftSound();
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isPlayerInside = true;
+            isLiftingUp = true;
+            delayTimer = delayTime;
+            PlayLiftSound();
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isPlayerInside = false;
+        }
+    }
+
+    private void PlayLiftSound()
+    {
+        if (liftAudioSource != null && isLiftingUp)
+        {
+            liftAudioSource.Play();
+        }
+    }
+
+    private void StopLiftSound()
+    {
+        if (liftAudioSource != null)
+        {
+            liftAudioSource.Stop();
+        }
+    }
+}
