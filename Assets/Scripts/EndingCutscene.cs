63f9eae438025836723e3581e02ea6d19f6b537c
Parven <parvendranofficial05@gmail.com>
2023-07-20 19:17:20 +0800

    Finish Ending Cutscene

---
+++ Assets/Scripts/EndingCutscene.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingCutscene : MonoBehaviour
+{
+    [SerializeField] private GameObject logoImage;
+    [SerializeField] private GameObject panel;
+    [SerializeField] private AudioSource endBGM;
+    [SerializeField] private float delayBeforePanelAnimation = 2.0f;
+    [SerializeField] private float delayBeforeLogoAnimation = 2.0f;
+
+    [SerializeField] private FootSteps playerFootsteps;
+    [SerializeField] private FirstPersonController playerMovement;
+    [SerializeField] private GameObject playerCursor;
+
+    private Animator logoAnim;
+    private Animator panelAnim;
+
+    private bool hasTransitioned = false;
+    private bool hasPlayed = false;
+
+    private void Start()
+    {
+        logoAnim = logoImage.GetComponent<Animator>();
+        panelAnim = panel.GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !hasPlayed)
+        {
+            StartCoroutine(PlayAnimationWithDelay());
+        }
+    }
+
+    private IEnumerator PlayAnimationWithDelay()
+    {
+        hasTransitioned = true;
+
+        yield return new WaitForSeconds(delayBeforePanelAnimation);
+
+        endBGM.Play();
+        panelAnim.SetBool("isFade", true);
+        playerCursor.SetActive(false);
+        playerFootsteps.enabled = false;
+        playerMovement.enabled = false;
+
+        yield return new WaitForSeconds(delayBeforeLogoAnimation);
+
+        logoAnim.SetBool("isFade", true);
+  
+        hasPlayed = true;
+    }
+}
