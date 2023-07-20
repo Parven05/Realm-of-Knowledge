63b9370e620ecea4d98c7171da631205766ab03c
Parven <parvendranofficial05@gmail.com>
2023-07-20 22:25:29 +0800

    fix audio bug

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,25 +1,15 @@
 using UnityEngine;
-using System.Collections;
+
 public class SoundManager : MonoBehaviour
 {
     [SerializeField] private AudioSource introSoundEffect;
-    [SerializeField] private AudioSource introSoundBGM;
-    [SerializeField] private float introDelay = 2f;
-    [SerializeField] private float fadeDuration = 2f;
+    [SerializeField] float introDelay = 22f;
 
     private bool isIntroPlayed = false;
 
     private void Start()
     {
         PlayIntroSoundEffectWithDelay();
-    }
-
-    private void Update()
-    {
-        if (isIntroPlayed && !introSoundEffect.isPlaying && !introSoundBGM.isPlaying)
-        {
-            StartCoroutine(FadeInBGM());
-        }
     }
 
     private void PlayIntroSoundEffectWithDelay()
@@ -31,21 +21,4 @@ public class SoundManager : MonoBehaviour
         }
     }
 
-    private IEnumerator FadeInBGM()
-    {
-        float currentTime = 0f;
-        float startVolume = 0f;
-
-        introSoundBGM.Play();
-        introSoundBGM.volume = startVolume;
-
-        while (currentTime < fadeDuration)
-        {
-            currentTime += Time.deltaTime;
-            introSoundBGM.volume = Mathf.Lerp(startVolume, 1f, currentTime / fadeDuration);
-            yield return null;
-        }
-
-        introSoundBGM.volume = 1f;
-    }
 }
