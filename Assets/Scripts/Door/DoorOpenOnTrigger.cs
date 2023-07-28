93c180038468041e72ce101c01b11690e54575ab
Parven <parvendranofficial05@gmail.com>
2023-07-28 22:30:21 +0800

    //

--- Assets/Scripts/Door/DoorOpenOnTrigger.cs
+++ Assets/Scripts/Door/DoorOpenOnTrigger.cs
@@ -9,20 +9,24 @@ public class DoorOpenOnTrigger : MonoBehaviour
     [SerializeField] private GameObject doorTrigger;
     [SerializeField] private AudioSource doorOpenSfx;
 
+    private bool doorOpened = false; // New flag to check if the door is already opened.
+
     private void Start()
     {
         doorTrigger.SetActive(false);
     }
+
     private void DoorOpen()
     {
-        doorTrigger.SetActive(true);
+        doorTrigger.SetActive(false);
         doorOpenSfx.Play();
         doorAnimator.SetBool("isOpen", true);
+        doorOpened = true; // Set the flag to true when the door is opened.
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (!doorOpened && other.CompareTag("Player")) // Check if the door is not already opened and if the player triggers the collider.
         {
             DoorOpen();
         }
--- Assets/Scripts/Puzzle Door/PuzzleDoorOpener.cs
+++ Assets/Scripts/Puzzle Door/PuzzleDoorOpener.cs
@@ -9,8 +9,9 @@ public class PuzzleDoorOpener : MonoBehaviour
 
     [Header("Required Object")]
     [SerializeField] private QuizCompletionHandler[] completionObjects;
-   
+
     private bool allCompleted = false;
+    private bool doorOpened = false; // New flag to check if the door is already opened.
 
     private void Start()
     {
@@ -33,8 +34,15 @@ public class PuzzleDoorOpener : MonoBehaviour
 
     private void DoorOpen()
     {
-        doorTrigger.SetActive(true);
-        doorOpenSfx.Play();
+        doorTrigger.SetActive(false);
+
+        // Play the door open sound only if the door is not already opened.
+        if (!doorOpened)
+        {
+            doorOpenSfx.Play();
+            doorOpened = true;
+        }
+
         doorAnimator.SetBool("isOpen", true);
     }
 
