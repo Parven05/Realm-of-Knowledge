cba4502e089b25a3d5f3ca1b565dcb321aae80ee
Parven <parvendranofficial05@gmail.com>
2023-07-31 02:14:03 +0800

    //

--- Assets/Scripts/ChangeTextColorUI.cs
+++ Assets/Scripts/ChangeTextColorUI.cs
@@ -8,10 +8,11 @@ public class ChangeTextColorUI : MonoBehaviour, IPointerEnterHandler, IPointerEx
     private TextMeshProUGUI buttonText;
     private Color originalColor;
     private Color hoverColor = Color.white;
+    private bool isHovered = false;
 
     private void Start()
     {
-        // Get the Text component of the button
+        // Get the TextMeshProUGUI component of the button
         buttonText = GetComponent<TextMeshProUGUI>();
 
         // Store the original color of the button
@@ -20,13 +21,18 @@ public class ChangeTextColorUI : MonoBehaviour, IPointerEnterHandler, IPointerEx
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        // When the pointer hovers over the button, change the text color to white
-        buttonText.color = hoverColor;
+        // When the pointer hovers over the button, change the text color to white only if it's not already in hover color
+        if (!isHovered)
+        {
+            buttonText.color = hoverColor;
+            isHovered = true;
+        }
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        // When the pointer exits the button, revert the text color to the original color
+        // When the pointer exits the button, revert the text color to the original color and reset the hover flag
         buttonText.color = originalColor;
+        isHovered = false;
     }
 }
